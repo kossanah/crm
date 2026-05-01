19f9d12f5e58ca86fcab32bc91ca7583c0ba6ec7
Kaosiso Anaekwe <kossanah@gmail.com>
2026-05-01 14:09:52 +0100

    fix: Add backward compatibility for Frappe v15 (modules.py import)
    - Add try/except block to handle missing frappe.utils.modules in Frappe v15
    - Provide fallback implementation for get_modules_from_all_apps_for_user
    - Resolves ModuleNotFoundError on Frappe v15.106.0

--- crm/api/__init__.py
+++ crm/api/__init__.py
@@ -3,148 +3,182 @@ from bs4 import BeautifulSoup
 from frappe.core.api.file import get_max_file_size
 from frappe.translate import get_all_translations
 from frappe.utils import cstr, split_emails, validate_email_address
-from frappe.utils.modules import get_modules_from_all_apps_for_user
 from frappe.utils.telemetry import POSTHOG_HOST_FIELD, POSTHOG_PROJECT_FIELD
+
+# Backward compatibility for Frappe v15 (modules.py was introduced in v16)
+try:
+    from frappe.utils.modules import get_modules_from_all_apps_for_user
+except ImportError:
+    # Fallback implementation for Frappe v15
+    def get_modules_from_all_apps_for_user(user=None):
+        """Backward compatible implementation for Frappe v15"""
+        user = user or frappe.session.user
+        all_modules = []
+
+        # Get all modules from all apps
+        for app in frappe.get_installed_apps():
+            modules = frappe.get_all("Module Def", filters={"app_name": app}, fields=[
+                                     "module_name", "app_name as app"])
+            all_modules.extend(modules)
+
+        # Filter blocked modules
+        if user and user != "Administrator":
+            try:
+                global_blocked = frappe.get_doc(
+                    "User", "Administrator").get_blocked_modules()
+                user_blocked = frappe.get_doc(
+                    "User", user).get_blocked_modules()
+                blocked_modules = global_blocked + user_blocked
+                all_modules = [m for m in all_modules if m.get(
+                    "module_name") not in blocked_modules]
+            except Exception:
+                pass  # If method doesn't exist, return all modules
+
+        return all_modules
 
 
 @frappe.whitelist(allow_guest=True)
 def get_translations():
-	if frappe.session.user != "Guest":
-		language = frappe.db.get_value("User", frappe.session.user, "language")
-	else:
-		language = frappe.db.get_single_value("System Settings", "language")
+    if frappe.session.user != "Guest":
+        language = frappe.db.get_value("User", frappe.session.user, "language")
+    else:
+        language = frappe.db.get_single_value("System Settings", "language")
 
-	return get_all_translations(language)
+    return get_all_translations(language)
 
 
 @frappe.whitelist()
 def get_user_signature():
-	user = frappe.session.user
-	user_email_signature = (
-		frappe.db.get_value(
-			"User",
-			user,
-			"email_signature",
-		)
-		if user
-		else None
-	)
+    user = frappe.session.user
+    user_email_signature = (
+        frappe.db.get_value(
+            "User",
+            user,
+            "email_signature",
+        )
+        if user
+        else None
+    )
 
-	signature = user_email_signature or frappe.db.get_value(
-		"Email Account",
-		{"default_outgoing": 1, "add_signature": 1},
-		"signature",
-	)
+    signature = user_email_signature or frappe.db.get_value(
+        "Email Account",
+        {"default_outgoing": 1, "add_signature": 1},
+        "signature",
+    )
 
-	if not signature:
-		return
+    if not signature:
+        return
 
-	soup = BeautifulSoup(signature, "html.parser")
-	html_signature = soup.find("div", {"class": "ql-editor read-mode"})
-	_signature = None
-	if html_signature:
-		_signature = html_signature.renderContents()
-	content = ""
-	if cstr(_signature) or signature:
-		content = f'<br><p class="signature">{signature}</p>'
-	return content
+    soup = BeautifulSoup(signature, "html.parser")
+    html_signature = soup.find("div", {"class": "ql-editor read-mode"})
+    _signature = None
+    if html_signature:
+        _signature = html_signature.renderContents()
+    content = ""
+    if cstr(_signature) or signature:
+        content = f'<br><p class="signature">{signature}</p>'
+    return content
 
 
 @frappe.whitelist()
 def get_posthog_settings():
-	return {
-		"posthog_project_id": frappe.conf.get(POSTHOG_PROJECT_FIELD),
-		"posthog_host": frappe.conf.get(POSTHOG_HOST_FIELD),
-		"enable_telemetry": frappe.get_system_settings("enable_telemetry"),
-		"telemetry_site_age": frappe.utils.telemetry.site_age(),
-	}
+    return {
+        "posthog_project_id": frappe.conf.get(POSTHOG_PROJECT_FIELD),
+        "posthog_host": frappe.conf.get(POSTHOG_HOST_FIELD),
+        "enable_telemetry": frappe.get_system_settings("enable_telemetry"),
+        "telemetry_site_age": frappe.utils.telemetry.site_age(),
+    }
 
 
 def check_app_permission():
-	if frappe.session.user == "Administrator":
-		return True
+    if frappe.session.user == "Administrator":
+        return True
 
-	allowed_modules = get_modules_from_all_apps_for_user()
-	allowed_modules = [x["module_name"] for x in allowed_modules]
-	if "FCRM" not in allowed_modules:
-		return False
+    allowed_modules = get_modules_from_all_apps_for_user()
+    allowed_modules = [x["module_name"] for x in allowed_modules]
+    if "FCRM" not in allowed_modules:
+        return False
 
-	roles = frappe.get_roles()
-	if any(
-		role in ["System Manager", "Sales User", "Sales Manager"] for role in roles
-	):
-		return True
+    roles = frappe.get_roles()
+    if any(
+            role in ["System Manager", "Sales User", "Sales Manager"] for role in roles
+    ):
+        return True
 
-	return False
+    return False
 
 
 @frappe.whitelist(allow_guest=True)
 def accept_invitation(key: str | None = None):
-	if not key:
-		frappe.throw("Invalid or expired key")
+    if not key:
+        frappe.throw("Invalid or expired key")
 
-	result = frappe.db.get_all("CRM Invitation", filters={"key": key}, pluck="name")
-	if not result:
-		frappe.throw("Invalid or expired key")
+    result = frappe.db.get_all("CRM Invitation", filters={
+                               "key": key}, pluck="name")
+    if not result:
+        frappe.throw("Invalid or expired key")
 
-	invitation = frappe.get_doc("CRM Invitation", result[0])
-	invitation.accept()
-	invitation.reload()
+    invitation = frappe.get_doc("CRM Invitation", result[0])
+    invitation.accept()
+    invitation.reload()
 
-	if invitation.status == "Accepted":
-		frappe.local.login_manager.login_as(invitation.email)
-		frappe.local.response["type"] = "redirect"
-		frappe.local.response["location"] = "/crm"
+    if invitation.status == "Accepted":
+        frappe.local.login_manager.login_as(invitation.email)
+        frappe.local.response["type"] = "redirect"
+        frappe.local.response["location"] = "/crm"
 
 
 @frappe.whitelist()
 def invite_by_email(emails: str, role: str):
-	frappe.only_for(["Sales Manager", "System Manager"])
+    frappe.only_for(["Sales Manager", "System Manager"])
 
-	if role not in ["System Manager", "Sales Manager", "Sales User"]:
-		frappe.throw("Cannot invite for this role")
+    if role not in ["System Manager", "Sales Manager", "Sales User"]:
+        frappe.throw("Cannot invite for this role")
 
-	if not emails:
-		return
+    if not emails:
+        return
 
-	email_string = validate_email_address(emails, throw=False)
-	email_list = split_emails(email_string)
-	if not email_list:
-		return
-	existing_members = frappe.db.get_all("User", filters={"email": ["in", email_list]}, pluck="email")
-	existing_invites = frappe.db.get_all(
-		"CRM Invitation",
-		filters={
-			"email": ["in", email_list],
-			"role": ["in", ["System Manager", "Sales Manager", "Sales User"]],
-		},
-		pluck="email",
-	)
+    email_string = validate_email_address(emails, throw=False)
+    email_list = split_emails(email_string)
+    if not email_list:
+        return
+    existing_members = frappe.db.get_all(
+        "User", filters={"email": ["in", email_list]}, pluck="email")
+    existing_invites = frappe.db.get_all(
+        "CRM Invitation",
+        filters={
+            "email": ["in", email_list],
+            "role": ["in", ["System Manager", "Sales Manager", "Sales User"]],
+        },
+        pluck="email",
+    )
 
-	to_invite = list(set(email_list) - set(existing_members) - set(existing_invites))
+    to_invite = list(set(email_list) -
+                     set(existing_members) - set(existing_invites))
 
-	for email in to_invite:
-		frappe.get_doc(doctype="CRM Invitation", email=email, role=role).insert(ignore_permissions=True)
+    for email in to_invite:
+        frappe.get_doc(doctype="CRM Invitation", email=email,
+                       role=role).insert(ignore_permissions=True)
 
-	return {
-		"existing_members": existing_members,
-		"existing_invites": existing_invites,
-		"to_invite": to_invite,
-	}
+    return {
+        "existing_members": existing_members,
+        "existing_invites": existing_invites,
+        "to_invite": to_invite,
+    }
 
 
 @frappe.whitelist()
 def get_file_uploader_defaults(doctype: str):
-	max_number_of_files = None
-	make_attachments_public = False
-	if doctype:
-		meta = frappe.get_meta(doctype)
-		max_number_of_files = meta.get("max_attachments")
-		make_attachments_public = meta.get("make_attachments_public")
+    max_number_of_files = None
+    make_attachments_public = False
+    if doctype:
+        meta = frappe.get_meta(doctype)
+        max_number_of_files = meta.get("max_attachments")
+        make_attachments_public = meta.get("make_attachments_public")
 
-	return {
-		"allowed_file_types": frappe.get_system_settings("allowed_file_extensions"),
-		"max_file_size": get_max_file_size(),
-		"max_number_of_files": max_number_of_files,
-		"make_attachments_public": bool(make_attachments_public),
-	}
+    return {
+        "allowed_file_types": frappe.get_system_settings("allowed_file_extensions"),
+        "max_file_size": get_max_file_size(),
+        "max_number_of_files": max_number_of_files,
+        "make_attachments_public": bool(make_attachments_public),
+    }
